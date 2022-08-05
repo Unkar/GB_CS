92be1d3ce40b31087ea3d3dd5ca03a4bb20b6295
Sergey <2805026@gmail.com>
2022-08-05 22:54:42 +0300

    seminar1. add homework task6

---
+++ Seminars/Seminar1/HWTask6/Program.cs
@@ -0,0 +1,31 @@
+﻿/* Задача 6: Напишите программу, которая на вход принимает число
+ и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+
+4 -> да
+-3 -> нет
+7 -> нет
+ */
+
+while (true)
+{
+    Console.WriteLine("Проверка на чётность. ");
+
+    Console.WriteLine("Введите число: ");
+    int number1 = Convert.ToInt32(Console.ReadLine());
+
+    if (number1 % 2 == 0)
+    {
+        Console.WriteLine("Число чётное");
+    }
+    else
+    {
+        Console.WriteLine("Число нечётное");
+    }
+
+    Console.WriteLine("Повторим ещё раз? (Y(anykey)/N)");
+    string continueRequest = Console.ReadLine();
+    if (continueRequest.ToLower() == "n")
+    {
+        break;
+    }
+}
