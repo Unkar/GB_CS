263fcc12a95cced2819890bd9a0350529d5958fb
Sergey <2805026@gmail.com>
2022-08-05 23:05:50 +0300

    seminar1. add homework task8

---
+++ Seminars/Seminar1/HWTask8/Program.cs
@@ -0,0 +1,31 @@
+﻿/* Задача 8: Напишите программу, которая на вход принимает число (N),
+ а на выходе показывает все чётные числа от 1 до N.
+
+5 -> 2, 4
+8 -> 2, 4, 6, 8 */
+
+while (true)
+{
+    Console.WriteLine("Вывести все чётные числа от 1 до N");
+
+    Console.WriteLine("Введите число N: ");
+    int numberN = Convert.ToInt32(Console.ReadLine());
+
+    int count = 2;
+
+    while (count <= numberN)
+    {
+        Console.Write(count);
+        Console.Write(" ");
+        count = count + 2;
+    }
+
+    Console.WriteLine();
+
+    Console.WriteLine("Повторим ещё раз? (Y(anykey)/N)");
+    string continueRequest = Console.ReadLine();
+    if (continueRequest.ToLower() == "n")
+    {
+        break;
+    }
+}
