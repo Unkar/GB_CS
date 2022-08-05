218544c8964a1ab05b792602407cb356c1484fd6
Sergey <2805026@gmail.com>
2022-08-05 19:40:36 +0300

    Home work from Lesson1 done. Dog and two men problem

---
+++ Lessons/Lesson1/Homework/Program.cs
@@ -0,0 +1,27 @@
+﻿double count = 0,
+    distance = 10000,
+    firstFriendSpeed = 1,
+    secondFriendSpeed = 2,
+    dogSpeed = 5,
+    friend = 2;
+double time;
+
+while (distance > 10)
+{
+    if (friend == 1)
+    {
+        time = distance / (secondFriendSpeed + dogSpeed);
+        friend = 2;
+    }
+    else
+    {
+        time = distance / (firstFriendSpeed + dogSpeed);
+        friend = 1;
+    }
+    distance = distance - (firstFriendSpeed + secondFriendSpeed) * time;
+    count++;
+}
+
+Console.Write("Собака пробежит ");
+Console.Write(count);
+Console.WriteLine(" раз между людьми");
