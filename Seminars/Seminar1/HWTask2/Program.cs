d0ba2bbf7f966e2092faf201cd689d81a4623801
Sergey <2805026@gmail.com>
2022-08-05 22:17:27 +0300

    seminar1. add hwtask2

---
+++ Seminars/Seminar1/HWTask2/Program.cs
@@ -0,0 +1,33 @@
+﻿while (true)
+{
+    Console.WriteLine("Сравнение двух чисел.");
+
+    Console.WriteLine("Введите первое число a: ");
+    int number1 = Convert.ToInt32(Console.ReadLine());
+
+    Console.WriteLine("Введите второе число b: ");
+    int number2 = Convert.ToInt32(Console.ReadLine());
+
+    if (number1 > number2)
+    {
+        Console.WriteLine("Наибольшее число: ");
+        Console.WriteLine(number1);
+
+        Console.WriteLine("Наименьшее число: ");
+        Console.WriteLine(number2);
+    }
+    else
+    {
+        Console.WriteLine("Наибольшее число: ");
+        Console.WriteLine(number2);
+
+        Console.WriteLine("Наименьшее число: ");
+        Console.WriteLine(number1);
+    }
+    Console.WriteLine("Повторим ещё раз? (Y/N)");
+    string continueRequest = Console.ReadLine();
+    if (continueRequest.ToLower() == "n")
+    {
+        break;
+    }
+}
