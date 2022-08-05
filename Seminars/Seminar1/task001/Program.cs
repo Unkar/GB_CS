c4b9d83d5b92d0ce75c0962d70cd2012529069a5
Sergey <2805026@gmail.com>
2022-08-05 21:10:50 +0300

    Add task001. Calculator

---
+++ Seminars/Seminar1/task001/Program.cs
@@ -0,0 +1,30 @@
+﻿// int string bool float double
+/* int a = 10;
+string b = "Hello";
+bool c = true; // false
+float d = 25.23;
+double e = 13.334; */
+
+Console.WriteLine("Введите первое число ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите оператор ");
+string Operator = Console.ReadLine();
+if (Operator == "+")
+{
+    Console.WriteLine(number1 + number2);
+}
+else if (Operator == "-")
+{
+    Console.WriteLine(number1 - number2);
+}
+else if (Operator == "*")
+{
+    Console.WriteLine(number1 * number2);
+}
+else if (Operator == "/")
+{
+    Console.WriteLine(number1 / number2);
+}
