9fd4eaccd269075ef305f546dd6e5091d382e0ba
Sergey <2805026@gmail.com>
2022-08-05 21:34:56 +0300

    task001. Add endless loop and possibility to exit the program

--- Seminars/Seminar1/task001/Program.cs
+++ Seminars/Seminar1/task001/Program.cs
@@ -5,26 +5,39 @@ bool c = true; // false
 float d = 25.23;
 double e = 13.334; */
 
-Console.WriteLine("Введите первое число ");
-int number1 = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Введите второе число ");
-int number2 = Convert.ToInt32(Console.ReadLine());
+while (true)
+{
+    Console.WriteLine("Введите оператор или end чтобы закончить ");
+    string Operator = Console.ReadLine();
 
-Console.WriteLine("Введите оператор ");
-string Operator = Console.ReadLine();
-if (Operator == "+")
-{
-    Console.WriteLine(number1 + number2);
+    if (Operator == "end")
+    {
+        break;
+    }
+
+    Console.WriteLine("Введите первое число ");
+    int number1 = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Введите второе число ");
+    int number2 = Convert.ToInt32(Console.ReadLine());
+
+
+    if (Operator == "+")
+    {
+        Console.WriteLine(number1 + number2);
+    }
+    else if (Operator == "-")
+    {
+        Console.WriteLine(number1 - number2);
+    }
+    else if (Operator == "*")
+    {
+        Console.WriteLine(number1 * number2);
+    }
+    else if (Operator == "/")
+    {
+        Console.WriteLine(number1 / number2);
+    }
+
 }
-else if (Operator == "-")
-{
-    Console.WriteLine(number1 - number2);
-}
-else if (Operator == "*")
-{
-    Console.WriteLine(number1 * number2);
-}
-else if (Operator == "/")
-{
-    Console.WriteLine(number1 / number2);
-}
+
+
