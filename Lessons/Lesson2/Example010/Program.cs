112868d6db950760185e8b43ed5d1f8fa670d098
Sergey <2805026@gmail.com>
2022-08-07 20:36:35 +0300

    Lesson2. Add Example010 file. Find number

---
+++ Lessons/Lesson2/Example010/Program.cs
@@ -0,0 +1,15 @@
+﻿int[] array = { 1, 2, 3, 90, 5, 6, 7, 8, 9, 90 };
+
+int n =array.Length;
+int find = 90;
+
+int index = 0;
+ while (index<n)
+ {
+    if (array[index]==find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index++;
+ }
