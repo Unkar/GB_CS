a8d133cdbe2d06ecd31994736d7f5874aab83671
Sergey <2805026@gmail.com>
2022-08-09 21:34:59 +0300

    Siminar2. RPG. Add class and male select

--- Seminars/Seminar2/Task002/Program.cs
+++ Seminars/Seminar2/Task002/Program.cs
@@ -1,20 +1,42 @@
 ﻿using System;
 class Program
 {
+    static void WriteSelect(string[] namesArray)
+    {
+        for (int i = 0; i < namesArray.Length - 1; i++)
+        {
+            Console.Write(i + 1 + " - " + namesArray[i] + ", ");
+        }
+        Console.WriteLine(namesArray.Length + " - " + namesArray[namesArray.Length - 1] + ".");
+    }
     static void SelectRace()
     {
         string[] namesRace = { "Люди", "Эльфы", "Гномы", "Орки" };
-        for (int i = 0; i < namesRace.Length - 1; i++)
-        {
-            Console.Write(i + 1 + " - " + namesRace[i] + ", ");
-        }
-        Console.Write(namesRace.Length + " - " + namesRace[namesRace.Length - 1] + ".");
+        WriteSelect(namesRace);
         Console.WriteLine("Укажите вашу расу");
         int indexRace = Convert.ToInt32(Console.ReadLine());
         Console.WriteLine("Ваша раса " + namesRace[indexRace - 1]);
     }
+    static void SelectClass()
+    {
+        string[] namesClass = { "Мечник", "Лучник", "Вор", "Маг" };
+        WriteSelect(namesClass);
+        Console.WriteLine("Укажите вашу расу");
+        int indexClass = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Ваша раса " + namesClass[indexClass - 1]);
+    }
+    static void SelectMale()
+    {
+        string[] namesMale = { "Мужчина", "Женщина"};
+        WriteSelect(namesMale);
+        Console.WriteLine("Укажите вашу расу");
+        int indexMale = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Ваша раса " + namesMale[indexMale - 1]);
+    }    
     static void Main(string[] args)
     {
         SelectRace();
+        SelectClass();
+        SelectMale();
     }
 }
