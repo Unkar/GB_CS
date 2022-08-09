87cca556ae3a21fed2db45e1c59c0be477c249ae
Sergey <2805026@gmail.com>
2022-08-09 21:11:31 +0300

    Siminar2. RPG. Add race select

---
+++ Seminars/Seminar2/Task002/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+class Program
+{
+    static void SelectRace()
+    {
+        string[] namesRace = { "Люди", "Эльфы", "Гномы", "Орки" };
+        for (int i = 0; i < namesRace.Length - 1; i++)
+        {
+            Console.Write(i + 1 + " - " + namesRace[i] + ", ");
+        }
+        Console.Write(namesRace.Length + " - " + namesRace[namesRace.Length - 1] + ".");
+        Console.WriteLine("Укажите вашу расу");
+        int indexRace = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Ваша раса " + namesRace[indexRace - 1]);
+    }
+    static void Main(string[] args)
+    {
+        SelectRace();
+    }
+}
