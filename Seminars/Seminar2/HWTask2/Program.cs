f2a5dbf68a4c57e2b535043f190999ebd60a80a1
Sergey <2805026@gmail.com>
2022-08-10 23:20:17 +0300

    Siminar2. Home work task 13 added

---
+++ Seminars/Seminar2/HWTask2/Program.cs
@@ -0,0 +1,30 @@
+﻿/* Задача 13: Напишите программу, которая выводит третью цифру заданного числа
+ или сообщает, что третьей цифры нет.
+
+645 -> 5
+
+78 -> третьей цифры нет
+
+32679 -> 6 */
+
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Введите трехзначное число:");
+        string number = Console.ReadLine();
+        char[] figure = number.ToCharArray();
+
+
+        if (figure.Length > 2)
+        {
+            Console.WriteLine(figure[2]);
+        }
+        else
+        {
+            Console.WriteLine("третьей цифры нет");
+        }
+    }
+}
