d3af8a5dcd57faa5881c402c47cb83fcf9773fd4
Sergey <2805026@gmail.com>
2022-09-03 21:22:08 +0300

    Seminar9. Classwork task 1

---
+++ Seminars/Seminar9/ClassWorkTask/Program.cs
@@ -0,0 +1,20 @@
+﻿//Напишите программу, которая будет принимать на вход 
+//число и возвращать сумму его цифр.
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Введите число");
+        int number = int.Parse(Console.ReadLine());
+        int sum = 0;
+        while (number > 0)
+        {
+            sum += number % 10;
+            number /= 10;
+        }
+        Console.WriteLine("Сумма цифр числа равна {0}", sum);
+        Console.ReadKey();
+    }
+}
+
